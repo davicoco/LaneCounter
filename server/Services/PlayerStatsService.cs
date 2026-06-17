bac251c64881a7d6127d55eb2423ce70c726f104
David Izquierdo Puentes <david_izqpue@hotmail.com>
2026-06-17 12:31:59 +0200

    Add scaffold PlayerStatsService and PlayerStatsDto

---
+++ server/Services/PlayerStatsService.cs
@@ -0,0 +1,11 @@
+using server.Models;
+
+namespace server.Services;
+
+public class PlayerStatsService
+{
+    public PlayerStatsDto CalculatePlayerStats(List<MatchDto> matches, string puuid)
+    {
+        throw new NotImplementedException();
+    }
+}
