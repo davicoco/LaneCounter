1381a6bd426f73800ed33101493942ebccfdfcd9
David Izquierdo Puentes <david_izqpue@hotmail.com>
2026-06-17 16:40:08 +0200

    Implement CalculatePlayerStats with KDA and winrate

--- server/Services/PlayerStatsService.cs
+++ server/Services/PlayerStatsService.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using server.Models;
 
 namespace server.Services;
@@ -6,6 +7,34 @@ public class PlayerStatsService
 {
     public PlayerStatsDto CalculatePlayerStats(List<MatchDto> matches, string puuid)
     {
-        throw new NotImplementedException();
+        if (matches.Count == 0)
+        {
+            return new PlayerStatsDto();
+        }
+        double totalKills = 0;
+        double totalDeaths = 0;
+        double totalAssists = 0;
+        double wins = 0;
+        foreach (var match in matches)
+        {
+            var player = match.Info.Participants.First((p) => p.Puuid == puuid);
+            totalKills += player.Kills;
+            totalDeaths += player.Deaths;
+            totalAssists += player.Assists;
+            if (player.Win)
+            {
+                wins++;
+            }
+
+        }
+        var stats = new PlayerStatsDto
+        {
+            AverageKills = totalKills / matches.Count,
+            AverageDeaths = totalDeaths / matches.Count,
+            AverageAssists = totalAssists / matches.Count,
+            RecentWinRate = wins / matches.Count
+        };
+
+        return stats;
     }
 }
